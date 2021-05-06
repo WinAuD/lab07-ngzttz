0d13e08609c34923912d6b1bcd2e7ae077415c82
WinAuD <mailchris1@gmx.de>
2021-05-06 09:36:33 +0200

    2 files modified

--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-
-import store from './store';
-import { Provider } from 'react-redux'
+import store from "./store";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
-   
-    <Provider store={store}>
-      <App />
-    </Provider>
-   ,
-  document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>
+  ,
+  document.getElementById("root")
 );
-
