d874232f8b09f9bd9913ba5beba31cf294bffc33
WinAuD <mailchris1@gmx.de>
2021-05-10 13:29:08 +0200

    2 files modified

--- src/reducer.js
+++ src/reducer.js
@@ -43,15 +43,9 @@ export default function fontSizeReducer(state = initialState, action) {
 
 // Thunk function
 export async function fetchWiki(dispatch, getState) {
-    alert("fetch");
+    // alert("fetch");
     const response = await axios.get(`https://de.wikipedia.org/w/api.php?action=query&origin=*&prop=extracts&format=json&titles=${getState().text}`) 
-alert("response" + response);
-
     const res = getTextFromAxios(response.data); 
-
-    alert("response" + response);
-    alert(res);
-
     dispatch({ type: 'fontSize/fetchWiki', payload: res })
 }
 
@@ -62,18 +56,3 @@ function getTextFromAxios(responseObj) {
   str = responseObj.query.pages[keyVal].extract; 
   return str; 
 }
-
-// // Write a synchronous outer function that receives the `text` parameter:
-// export function fetchWikiWithText(text) {
-//   // Thunk function
-//   return async function fetchWiki(dispatch, getState) {
-//     alert("fetch");
-//     const response = await axios.get(`https://de.wikipedia.org/w/api.php?action=query&origin=*&prop=extracts&format=json&titles=${getState().text}`)
-//     const res = getTextFromAxios(response.data);
-
-//     alert("res");
-//     alert(res);
-
-//     dispatch({ type: 'fontSize/fetchWiki', payload: res })
-//   }
-// }
